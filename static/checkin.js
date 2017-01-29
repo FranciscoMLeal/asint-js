3845aa1ce79a6c474c51f81fb950a2e439eff679
FranciscoMLeal <francisco.leal@ist.utl.pt>
2017-01-29 21:29:49 +0000

    added CSS and optimized code

--- static/checkin.js
+++ static/checkin.js
@@ -1,83 +1,84 @@
 
 function getAllRooms(){
-  var getRooms = new XMLHttpRequest();
-  var url = "https://fenix-rooms.herokuapp.com/api/rooms";
-  getRooms.open("GET", url, true);
+    var getRooms = new XMLHttpRequest();
+    var url = "https://fenix-rooms.herokuapp.com/api/rooms";
+    getRooms.open("GET", url, true);
 
-  getRooms.onreadystatechange = function(){
-    if(this.readyState == 4){
-      if(this.status == 200){
-        var i;
-        var parsedJson = JSON.parse(getRooms.responseText);
-        var div = document.querySelector("#container"),
-        radiobutton = document.createElement("input"),
-        textRadio = document.createTextNode(""),
-        newline,
-        button;
+    getRooms.onreadystatechange = function(){
+        if(this.readyState == 4){
+            if(this.status == 200){
 
-        for (i=0; i < parsedJson.length; i++){
-          radiobutton = document.createElement("input");
-          radiobutton.setAttribute("type","radio");
-          radiobutton.setAttribute("name","salas");
-          radiobutton.setAttribute("id",parsedJson[i].fenix_id);
-          radiobutton.setAttribute("value",parsedJson[i].fenix_id);
-          div.appendChild(radiobutton);
+            var parsedJson  = JSON.parse(getRooms.responseText);
+            var listofrooms = document.getElementById("listofrooms");
 
-          textRadio = document.createTextNode(parsedJson[i].location);
-          div.appendChild(textRadio);
+            for (var i=0; i < parsedJson.length; i++){
 
-          newline= document.createElement("br");
-          div.appendChild(newline);
-        }
-        button = document.createElement("button");
-        button.setAttribute("type", "button");
-        button.innerHTML = "Check In";
-        button.onclick = function(){
-          var search;
-          for(var i=0; i < parsedJson.length; i++){
-            if(document.getElementById(parsedJson[i].fenix_id).checked){
-              var user_id;
-              var cookieAux = document.cookie.split(';');
-              for(var j = 0; j < cookieAux.length; j++){
-                valCookieAux = cookieAux[j].split('=');
-                valCookieAux[0] = valCookieAux[0].replace(/\s+/g, '');
-                if (valCookieAux[0] == "userId"){
-                  user_id = valCookieAux[1];
-                }
-              }
-              if (user_id)
-              {
-                userChecksIn(user_id,parsedJson[i].fenix_id);
-              }else{
-                alert("couldnt checkin.");
-              }
-              break;
+                var radiobutton = document.createElement("input");
+                var textRadio   = document.createElement("label");
+                var newline     = document.createElement("li");
+
+                radiobutton = document.createElement("input");
+                radiobutton.setAttribute("type","radio");
+                radiobutton.setAttribute("name","salas");
+                radiobutton.setAttribute("id",parsedJson[i].fenix_id);
+                radiobutton.setAttribute("class","radio");
+                radiobutton.setAttribute("value",parsedJson[i].fenix_id);
+
+                textRadio.setAttribute("for",parsedJson[i].fenix_id);
+                textRadio.innerHTML = parsedJson[i].location;
+
+                newline.appendChild(radiobutton);
+                newline.appendChild(textRadio);
+
+                listofrooms.appendChild(newline);
             }
-          }
+
+
+            var button = document.getElementById("reserveroombtn");
+            button.className = "";
+
+            }
         }
-        div.appendChild(button);
-      }
-    }
   }
   getRooms.send();
 }
 
-function userChecksIn(user,room_id){
-  var CheckIn = new XMLHttpRequest();
-  var create_url = "https://fenix-rooms.herokuapp.com/api/check_in";
-  CheckIn.open("POST", create_url, true);
-  CheckIn.setRequestHeader("Content-type", "application/json");
-  CheckIn.onreadystatechange = function(){
-    if(this.readyState == 4){
-      if(this.status == 200){
-        alert("Checkin sucessfully");
-      }else{
-        alert("Couldn't checkin.")
-      }
+
+checkInRoom = function(){
+
+    var cookieAux = document.cookie.split(';');
+    var user_id   = cookieAux[1];
+    var room_id   = document.querySelector(':checked').value;
+
+    var errormessagediv   = document.getElementById("errormessage");
+    var successmessagediv = document.getElementById("successmessage");
+
+    var CheckIn = new XMLHttpRequest();
+    var create_url = "https://fenix-rooms.herokuapp.com/api/check_in";
+
+    CheckIn.open("POST", create_url, true);
+    CheckIn.setRequestHeader("Content-type", "application/json");
+
+    CheckIn.onreadystatechange = function(){
+        if(this.readyState == 4){
+            if(this.status == 200){
+                document.body.scrollTop = document.documentElement.scrollTop = 0;
+                successmessagediv.innerHTML = "Check in Success!";
+                fadein(successmessagediv);
+            }else{
+                document.body.scrollTop = document.documentElement.scrollTop = 0;
+                errormessagediv.innerHTML = "Check in Failed!";
+                fadein(errormessagediv);
+            }
+        }
     }
-  }
-  CheckIn.send(JSON.stringify({"user_id" : ""+user,"room_id" : ""+room_id},null,' '));
-  return false;
+
+    CheckIn.send(JSON.stringify({
+        "user_id" : ""+user_id,
+        "room_id" : ""+room_id
+    },null,' '));
+
+    return;
 }
 
 window.onload = getAllRooms;
