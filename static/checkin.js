f37596314b03bad754d1675f359eef0fb9a84543
FranciscoMLeal <francisco.leal@ist.utl.pt>
2017-01-30 00:17:32 +0000

    fixed 0% issue

--- static/checkin.js
+++ static/checkin.js
@@ -1,100 +1,116 @@
 
 function getAllRooms(){
-    var getRooms = new XMLHttpRequest();
-    var url = "https://fenix-rooms.herokuapp.com/api/rooms";
-    getRooms.open("GET", url, true);
-    getRooms.setRequestHeader('Access-Control-Allow-Origin', '*');
+  var getRooms = new XMLHttpRequest();
+  var url = "https://fenix-rooms.herokuapp.com/api/rooms";
+  getRooms.open("GET", url, true);
+  getRooms.setRequestHeader('Access-Control-Allow-Origin', '*');
 
-    getRooms.onreadystatechange = function(){
-      if(this.readyState == 4){
-        if(this.status == 200){
-          var parsedJson  = JSON.parse(getRooms.responseText);
-          var listofrooms = document.getElementById("listofrooms");
+  getRooms.onreadystatechange = function(){
+    if(this.readyState == 4){
+      if(this.status == 200){
+        var parsedJson  = JSON.parse(getRooms.responseText);
+        var listofrooms = document.getElementById("listofrooms");
 
-          for (var i=0; i < parsedJson.length; i++){
+        for (var i=0; i < parsedJson.length; i++){
 
-            var radiobutton = document.createElement("input");
-            var textRadio   = document.createElement("label");
-            var newline     = document.createElement("li");
+          var radiobutton = document.createElement("input");
+          var textRadio   = document.createElement("label");
+          var newline     = document.createElement("li");
 
-            radiobutton = document.createElement("input");
-            radiobutton.setAttribute("type","radio");
-            radiobutton.setAttribute("name","salas");
-            radiobutton.setAttribute("id",parsedJson[i].fenix_id);
-            radiobutton.setAttribute("class","radio");
-            radiobutton.setAttribute("value",parsedJson[i].fenix_id);
+          radiobutton = document.createElement("input");
+          radiobutton.setAttribute("type","radio");
+          radiobutton.setAttribute("name","salas");
+          radiobutton.setAttribute("id",parsedJson[i].fenix_id);
+          radiobutton.setAttribute("class","radio");
+          radiobutton.setAttribute("value",parsedJson[i].fenix_id);
 
-            textRadio.setAttribute("for",parsedJson[i].fenix_id);
-            var occupationPercentage = (parsedJson[i].fill*1.0) / (parsedJson[i].capacity*1.0) * 100;
-            occupationPercentage = Math.round(occupationPercentage * 100) / 100
-            if (occupationPercentage == 0.0 || parsedJson[i].capacity == "0"){
-              textRadio.innerHTML = parsedJson[i].location + " (100%)";
-            }else{
-              textRadio.innerHTML = parsedJson[i].location + " (" + occupationPercentage + "%)";
-            }
-
-            newline.appendChild(radiobutton);
-            newline.appendChild(textRadio);
-
-            listofrooms.appendChild(newline);
+          textRadio.setAttribute("for",parsedJson[i].fenix_id);
+          var occupationPercentage = (parsedJson[i].fill*1.0) / (parsedJson[i].capacity*1.0) * 100;
+          occupationPercentage = Math.round(occupationPercentage * 100) / 100
+          if (parsedJson[i].capacity == "0"){
+            textRadio.innerHTML = parsedJson[i].location + " (100%)";
+          }else{
+            textRadio.innerHTML = parsedJson[i].location + " (" + occupationPercentage + "%)";
           }
 
+          newline.appendChild(radiobutton);
+          newline.appendChild(textRadio);
 
-          var button = document.getElementById("reserveroombtn");
-          button.className = "";
-
+          listofrooms.appendChild(newline);
         }
+
+      }else{
+        var errormessagediv   = document.getElementById("errormessage");
+        errormessagediv.innerHTML = "Connection error!";
+        fadein(errormessagediv);
       }
+    }
   }
   getRooms.send();
 }
 
 checkInRoom = function(){
 
-    var cookieAux = document.cookie.split(';');
-    var user_id;
-    for(var i = 0; i < cookieAux.length; i++){
-      cookieAux[i] = cookieAux[i].replace(/\s+/g, '');
-      var cookieBrokenDown = cookieAux[i].split('=');
-      if (cookieBrokenDown[0] == "userId"){
-        user_id = cookieBrokenDown[1];
+  var cookieAux = document.cookie.split(';');
+  var user_id;
+
+  for(var i = 0; i < cookieAux.length; i++){
+    cookieAux[i] = cookieAux[i].replace(/\s+/g, '');
+    var cookieBrokenDown = cookieAux[i].split('=');
+    if (cookieBrokenDown[0] == "userId"){
+      user_id = cookieBrokenDown[1];
+    }
+  }
+
+  var room_id   = document.querySelector(':checked').value;
+
+  var errormessagediv   = document.getElementById("errormessage");
+  var successmessagediv = document.getElementById("successmessage");
+
+  var CheckIn = new XMLHttpRequest();
+  var create_url = "https://fenix-rooms.herokuapp.com/api/check_in";
+
+  CheckIn.open("POST", create_url, true);
+  CheckIn.setRequestHeader("Content-type", "application/json");
+  CheckIn.setRequestHeader('Access-Control-Allow-Origin', '*');
+
+  CheckIn.onreadystatechange = function(){
+    if(this.readyState == 4){
+      if(this.status == 200){
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
+        successmessagediv.innerHTML = "Check in Success!";
+				document.cookie = "userId=" + user_id;
+				document.cookie = "roomId=" + room_id;
+				window.location.href = "checkout.html";
+        fadein(successmessagediv);
+      }else{
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
+        errormessagediv.innerHTML = "Check in Failed!";
+        fadein(errormessagediv);
       }
     }
-    var room_id   = document.querySelector(':checked').value;
+  }
 
-    var errormessagediv   = document.getElementById("errormessage");
-    var successmessagediv = document.getElementById("successmessage");
+  CheckIn.send(JSON.stringify({
+    "user_id" : ""+user_id,
+    "room_id" : ""+room_id
+  },null,' '));
 
-    var CheckIn = new XMLHttpRequest();
-    var create_url = "https://fenix-rooms.herokuapp.com/api/check_in";
-
-    CheckIn.open("POST", create_url, true);
-    CheckIn.setRequestHeader("Content-type", "application/json");
-    CheckIn.setRequestHeader('Access-Control-Allow-Origin', '*');
-
-    CheckIn.onreadystatechange = function(){
-        if(this.readyState == 4){
-            if(this.status == 200){
-                document.body.scrollTop = document.documentElement.scrollTop = 0;
-                successmessagediv.innerHTML = "Check in Success!";
-  							document.cookie = "userId=" + user_id;
-  							document.cookie = "roomId=" + room_id;
-  							window.location.href = "checkout.html";
-                fadein(successmessagediv);
-            }else{
-                document.body.scrollTop = document.documentElement.scrollTop = 0;
-                errormessagediv.innerHTML = "Check in Failed!";
-                fadein(errormessagediv);
-            }
-        }
-    }
-
-    CheckIn.send(JSON.stringify({
-        "user_id" : ""+user_id,
-        "room_id" : ""+room_id
-    },null,' '));
-
-    return;
+  return;
 }
+
+function testbutton(){
+  if ( document.querySelector(':checked') )
+  {
+    document.getElementById("reserveroombtn").className = "";
+    fadeout(document.getElementById("errormessage"));
+    fadeout(document.getElementById("successmessage"));
+  }
+  else
+  {
+    document.getElementById("reserveroombtn").className = "btn-disable";
+  }
+}
+
 
 window.onload = getAllRooms;
--- static/common.js
+++ static/common.js
@@ -1,29 +1,29 @@
 function fadein(element) {
-    if ( element.style.display == ""){
-        var op = 0.1;  // initial opacity
-        element.style.display = 'block';
-        var timer = setInterval(function () {
-            if (op >= 1){
-                clearInterval(timer);
-            }
-            element.style.opacity = op;
-            element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-            op += op * 0.1;
-        }, 10);
-    }
+  if ( element.style.display == "" || element.style.display == "none" ){
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+      if (op >= 1){
+          clearInterval(timer);
+      }
+      element.style.opacity = op;
+      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+      op += op * 0.1;
+    }, 10);
+  }
 }
 
 function fadeout(element) {
-    if ( element.style.display == "block"){
-        var op = 1;  // initial opacity
-        var timer = setInterval(function () {
-            if (op <= 0.1){
-                clearInterval(timer);
-                element.style.display = 'none';
-            }
-            element.style.opacity = op;
-            element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-            op -= op * 0.1;
-        }, 50);
-    }
+  if ( element.style.display == "block"){
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+      if (op <= 0.1){
+          clearInterval(timer);
+          element.style.display = 'none';
+      }
+      element.style.opacity = op;
+      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+      op -= op * 0.1;
+    }, 10);
+  }
 }
